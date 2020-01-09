567a7bfdf563144d1412683625e263004a3d37f0
RobertFTW <mentalcic@gmail.com>
2020-01-09 20:21:23 +0100

    changed study drill for ex11

--- ex11_study_drill.py
+++ ex11_study_drill.py
@@ -1,3 +1,16 @@
 varijabla = input("Upisi nesto: ")
-print(varijabla)
-print(type(varijabla))
+print("Upisa ga ti za sve pare -", varijabla)
+#print(type(varijabla))
+#tip = type(varijabla)
+
+
+
+try:
+    val = int(varijabla)
+    print("Input is an integer number. Number = ", val)
+except ValueError:
+    try:
+        val = float(varijabla)
+        print("Input is a float number. Number = ", val)
+    except ValueError:
+      print("No.. input is not a number. It's a string")
