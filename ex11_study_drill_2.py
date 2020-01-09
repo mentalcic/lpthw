c028cc177375e5fcca0a9aeafd70d115734dcfe6
RobertFTW <mentalcic@gmail.com>
2020-01-09 21:53:10 +0100

    added comments and details to ex11sd2

--- ex11_study_drill_2.py
+++ ex11_study_drill_2.py
@@ -1,10 +1,26 @@
+# Assignment:
 # Accept two int values from the user and return their product.
 # If the product is greater than 1000, then return their sum
-
+#
 # Expected output
 # Enter first number 10
 # Enter second number 20
 # The result is 200
+
+# Solution
+#
+# def multiplication_or_sum(num1, num2):
+#   product = num1 *num2
+#   if(product < 1000):
+#     return product
+#   else:
+#     return num1 +num2
+#
+# number1 = int(input("Enter first number "))
+# number2 = int(input("Enter second number"))
+#
+# result = multiplication_or_sum(number1, number2)
+# print("The result is", result)
 
 
 number1 = input("Enter first number: ")
