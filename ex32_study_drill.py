48b849001ab095521136bc86aefe7f995be76cf1
RobertFTW <mentalcic@gmail.com>
2021-01-08 21:38:32 +0100

    Added more files

---
+++ ex32_study_drill.py
@@ -0,0 +1,6 @@
+# 1. Take a look at how you used range. Look up the range function to understand it.
+
+# 2. Could you have avoided that for-loop entirely on line 22 and just assigned range(0,6) directly to elements?
+
+# 3. Find the Python documentation on lists and read about them. What other operations can you do to lists besides
+# append?
